bd18bbf4a7be7f68d77b861226d5501ca173de00
Edriel1 <edriel10gama10@gmail.com>
2026-09-12 01:03:00 -0300

    tela app gerenciador_tarefas

--- app_gerenciador_tarefas/app/src/main/java/com/example/app_gerenciador_tarefas/MainActivity.kt
+++ app_gerenciador_tarefas/app/src/main/java/com/example/app_gerenciador_tarefas/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.app_gerenciador_tarefas.ui.theme.App_gerenciador_tarefasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +29,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             App_gerenciador_tarefasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CongratulationsWindow(
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
             }
@@ -31,10 +40,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CongratulationsWindow(modifier: Modifier = Modifier) {
+
+    val congratsImage = painterResource(R.drawable.ic_task_completed)
+    val explanationText = stringResource(R.string.explanetion_text)
+    val congratsText = stringResource(R.string.Congratulations_text)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
+    ) {
+        CompleteImage(image = congratsImage)
+        TextExplanetion(text = explanationText)
+        TextCongratulations(text = congratsText)
+    }
+}
+
+@Composable
+fun CompleteImage(image: Painter, modifier: Modifier = Modifier) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TextExplanetion(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+    )
+}
+
+@Composable
+fun TextCongratulations(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        fontSize = 16.sp,
+        modifier = Modifier
     )
 }
 
@@ -42,6 +88,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     App_gerenciador_tarefasTheme {
-        Greeting("Android")
+        CongratulationsWindow(modifier = Modifier.fillMaxSize())
     }
 }
